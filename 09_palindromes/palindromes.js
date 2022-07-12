2269a227e40931897acd75c6dcf97aca5b59c2a7
mmeah21 <maevms@live.com>
2022-07-12 15:29:24 -0400

    Finished Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,20 @@
 const palindromes = function (array) {
 let original = array.toLowerCase();
+//console.log("Original Array: "+original);
 original = removeSpaces(original);
-let backwards = original.reverse();
+console.log("removed grammar original: "+original);
+let backwards = turn(original);
+console.log("backwards Array: "+backwards);
 let flag = true;
 
 for (var i =0; i<original.length;i++){
-    if(original[i]==backwards[i])
+    if(original[i]===backwards[i]){
+        //console.log("Original ["+i+"]"+original[i]+" compare "+"Backwards["+i+"]"+backwards[i])
         flag = true;
-        else{
+    }
+        else if(original[i]!= backwards[i]){
             flag = false;
+            console.log(flag);
             break;
         }
     }
@@ -19,12 +25,16 @@ function removeSpaces(array){
     for (var i =0; i<array.length;i++){
         if(array[i]!= ' ' && array[i]!= ',' && array[i]!= '.' && array[i]!= '!' && array[i]!= '?' && array[i]!= '\'' && array[i]!= '"')
         removed.push(array[i]);
-        else
-        i++;
     }
     return removed;
 }
-
+function turn(array){
+    let reverse = [];
+    for (var i = array.length-1; i>=0;i--){
+        reverse.push(array[i]);
+    }
+    return reverse;
+}
 
 //pass an array of objects ignoring punctuation and space.
 //convert all to lowercase
