67b2bbee627974032393685be9e1ea67438e6d4f
mmeah21 <maevms@live.com>
2022-07-12 08:47:11 -0400

    Solved 08 Troubleshooting 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,38 @@
-const add = function() {
-	
+const add = function(first,second) {
+	return first+second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first,second) {
+	return first-second;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let length = array.length;
+	let sum = 0;
+  for(var i =0; i < length;i++){
+    sum = sum+array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let numbers = array;
+  let product = 1;
+  for(var i =0; i < numbers.length;i++){
+    product = product*numbers[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return Math.pow(base, power)
 };
 
-const factorial = function() {
+const factorial = function fac(number) {
+  if(number <=1)
+    return 1;
+    else
+    return number*fac(number-1);
 	
 };
 
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,36 @@
-const palindromes = function () {
+const palindromes = function (array) {
+let original = array.toLowerCase();
+original = removeSpaces(original);
+let backwards = original.reverse();
+let flag = true;
 
+for (var i =0; i<original.length;i++){
+    if(original[i]==backwards[i])
+        flag = true;
+        else{
+            flag = false;
+            break;
+        }
+    }
+    return flag;
 };
+function removeSpaces(array){
+    let removed=[];
+    for (var i =0; i<array.length;i++){
+        if(array[i]!= ' ' && array[i]!= ',' && array[i]!= '.' && array[i]!= '!' && array[i]!= '?' && array[i]!= '\'' && array[i]!= '"')
+        removed.push(array[i]);
+        else
+        i++;
+    }
+    return removed;
+}
 
+
+//pass an array of objects ignoring punctuation and space.
+//convert all to lowercase
+//assign reverse array
+//compare the two arrays
+
+//ignore uppercase, punctuation and space
 // Do not edit below this line
 module.exports = palindromes;
